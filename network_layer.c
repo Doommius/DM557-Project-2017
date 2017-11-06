132d2316b808dbc2d9840c62a1e7ceff979fc4c5
Morten Jæger <mojae15@imada.sdu.dk>
2017-11-06 14:35:29 +0100

    Lavet noget med datagram

--- network_layer.c
+++ network_layer.c
@@ -15,51 +15,58 @@
 #include "network_layer.h"
 
 
-FifoQueue from_network_layer_queue;                /* Queue for data from network layer */
-FifoQueue for_network_layer_queue;    /* Queue for data for the network layer */
+FifoQueue queue_TtoN;   //Queue from Transport layer to Network Layer
+FifoQueue queue_NtoT;   //Queue from Network Layer to Transport Layer
 
+
+
+/* Make sure all locks and queues are initialized properly */
 void initialize_locks_and_queues(){
 
-    from_network_layer_queue = InitializeFQ();
-    for_network_layer_queue = InitializeFQ();
+    queue_TtoN = InitializeFQ();
+    queue_NtoT= InitializeFQ();
 }
 
+/* Where should a datagram be sent to. Transport layer knows only end hosts,
+ * but we will need to inform the link layer where it should forward it to */
 int forward(int toAddress){
 
 }
 
+/* Listen to relevant events for network layer, and act upon them */
 void network_layer_main_loop(){
     long int events_we_handle = network_layer_allowed_to_send | data_from_transport_layer | data_for_link_layer | data_from_link_layer;
     event_t event;
     FifoQueueEntry e;
+    datagram d;
 
     FifoQueue for_queue;
     FifoQueue from_queue;
-
 
 
     while (true) {
         Wait(&event, events_we_handle);
         switch (event.type) {
             case network_layer_allowed_to_send:
+
                 break;
 
             case data_from_link_layer:
 
+
                 break;
             case data_from_transport_layer:
-                printf("Fik signal, dequeuer:\n");
-                for_queue = (FifoQueue) get_for_network_layer_queue();
-                from_queue = (FifoQueue) get_from_network_layer_queue();
-                while (!EmptyFQ(for_queue) ){
-                    e = DequeueFQ(for_queue);
+                printf("Fik signal, laver datagram:\n");
+                for_queue = (FifoQueue) get_queue_TtoN();
+                from_queue = (FifoQueue) get_queue_NtoT();
 
-                    //TODO Vi skal ikke queue det igen, det er bare for at teste at vi kan sende tilbage til transport laget
-                    EnqueueFQ(e, from_queue);
-                }
+                e = DequeueFQ(for_queue);
+                strcpy(d.data, e->val);
+                printf("Datagram data: %s\n", d.data);
+
                 //printf("First: %s\n Last: %s\n", (char *) for_queue->first->val, (char *) for_queue->last->val);
                 //TODO Det her skal ikke være her, det er bare en test
-                printf("Signaler transport laget\n");
+                printf("Signalerer transport laget\n");
                 Signal(data_for_transport_layer, NULL);
 
                 break;
@@ -68,6 +75,9 @@ void network_layer_main_loop(){
     }
 }
 
+/* If there is data that should be sent, this function will check that the
+ * relevant queue is not empty, and that the link layer has allowed us to send
+ * to the neighbour  */
 void signal_link_layer_if_allowed(int address){
 
 }
@@ -132,10 +142,11 @@ void disable_network_layer(int station, boolean network_layer_allowance_list[], 
 //TODO Bedre funktions navne
 //TODO But how? aren't they descriptive as they are?
 
-FifoQueue *get_from_network_layer_queue(){
-    return (FifoQueue *) from_network_layer_queue;
+FifoQueue *get_queue_NtoT(){
+    return (FifoQueue *) queue_NtoT;
 }
 
-FifoQueue *get_for_network_layer_queue(){
-    return (FifoQueue *) for_network_layer_queue;
+FifoQueue *get_queue_TtoN(){
+    return (FifoQueue *) queue_TtoN;
 }
+
