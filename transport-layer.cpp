a1758d7fcd4c5015e46566f4ad69a8dea91302fc
Mark Jervelund <mark@jervelund.com>
2017-11-21 10:10:46 +0100

    Added some #define to transport-layer.h

--- transport-layer.cpp
+++ transport-layer.cpp
@@ -2,3 +2,64 @@
 // Created by jervelund on 11/17/17.
 //
 
+
+
+#include "transport-layer.h"
+#include "network_layer.h"
+
+int listen(transport_address local_address);
+
+
+/*
+ * Try to connect to remote host, on specified transport_address,
+ * listening back on local transport address. (full duplex).
+ *
+ * Once you have used the connection to send(), you can then be able to receive
+ * Returns the connection id - or an appropriate error code
+ */
+int connect(host_address remote_host, transport_address local_ta, transport_address remote_ta){
+
+    return 0;
+}
+
+/*
+ * Disconnect the connection with the supplied id.
+ * returns appropriate errorcode or 0 if successfull
+ */
+int disconnect(int connection_id){
+
+
+
+    return 0;
+}
+
+/*
+ * Set up a connection, so it is ready to receive data on, and wait for the main loop to signal all data received.
+ * Could have a timeout for a connection - and cleanup afterwards.
+ */
+int receive(char, unsigned char *, unsigned int *){
+
+
+    return 0;
+}
+
+/*
+ * On connection specified, send the bytes amount of data from buf.
+ * Must break the message down into chunks of a manageble size, and queue them up.
+ */
+int send(int connection_id, unsigned char *buf, unsigned int bytes){
+
+
+    return 0;
+}
+
+
+/*
+ * Main loop of the transport layer, handling the relevant events, fx. data arriving from the network layer.
+ * And take care of the different types of packages that can be received
+ */
+void transport_layer_loop(void){
+
+
+    return;
+}
